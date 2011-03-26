0b9613f5f43206e724d2f5bfc68f1d5d8be4eadf
James J Raden <james.raden@acxiom.com>
2011-03-25 22:27:35 -0400

    Add books.api.js and move the GetAuthors function to it.
    Signed-off-by: James J Raden <james.raden@acxiom.com>

---
+++ Scripts/Api/books.api.js
@@ -0,0 +1,14 @@
+﻿var booksApi = function () { }
+
+booksApi.prototype.getAuthors = function () {
+    $.ajax({
+        url: '/Api/Books/GetAuthors',
+        type: 'GET',
+        success: function (result) {
+            for (var i = 0; i < result.length; i++) {
+                var option = new Option(result[i], result[i]);
+                $('#authors').append(option);
+            }
+        }
+    });
+}
