eb1ccd410aa0e16eefbe28e919601db050d279b4
Jim Raden <jraden@acxiom.com>
2011-03-25 22:00:05 -0400

    Refactored to create a BooksController an the Api area for the Ajax calls.

---
+++ Areas/Api/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1WithAjax.Code;
+
+namespace MvcApplication1WithAjax.Areas.Api.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ActionResult GetAuthors()
+        {
+            List<string> authors = _bookRepository.GetAuthors();
+            return Json(authors, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult GetBooksForAuthor(string author)
+        {
+            List<Book> books = _bookRepository.GetBooksByAuthor(author);
+            return Json(books);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,13 +9,6 @@ namespace MvcApplication1WithAjax.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IBookRepository _bookRepository;
-
-        public HomeController()
-        {
-            _bookRepository = new BookRepository();
-        }
-
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -31,19 +24,6 @@ namespace MvcApplication1WithAjax.Controllers
         public ActionResult Books()
         {
             return View();
-        }
-
-        public ActionResult GetAuthors()
-        {
-            List<string> authors = _bookRepository.GetAuthors();
-            return Json(authors, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public ActionResult GetBooksForAuthor(string author)
-        {
-            List<Book> books = _bookRepository.GetBooksByAuthor(author);
-            return Json(books);
         }
     }
 }
