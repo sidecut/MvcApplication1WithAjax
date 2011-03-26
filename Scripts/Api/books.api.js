af0062f5c13d97bb73e0f2bbfaf9204ad7f28c94
Jim Raden <jraden@acxiom.com>
2011-03-25 22:43:00 -0400

    Moved get getAuthors() success function to a function parameter.

--- Scripts/Api/books.api.js
+++ Scripts/Api/books.api.js
@@ -1,15 +1,10 @@
 ﻿var booksApi = function () { }
 
-booksApi.prototype.getAuthors = function () {
+booksApi.prototype.getAuthors = function (success) {
     $.ajax({
         url: '/Api/Books/GetAuthors',
         type: 'GET',
-        success: function (result) {
-            for (var i = 0; i < result.length; i++) {
-                var option = new Option(result[i], result[i]);
-                $('#authors').append(option);
-            }
-        }
+        success: success
     });
 }
 
